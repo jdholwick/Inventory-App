105e31b84e0184d2168728121155d56619ad664e
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-12-09 14:23:52 -0700

    Item detail screen is now working for each item. I am working on setting up the view of each fragment. Next will be setting up the sell and delete buttons.

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -21,8 +21,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -35,28 +38,51 @@ class ItemDetailFragment : Fragment() {
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
 
+    lateinit var item: Item
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
+
+    // Takes an instance of 'Item' entity as parameter and returns nothing
+    private fun bind(item: Item) {
+        binding.apply {
+            binding.itemName.text = item.itemName
+            binding.itemPrice.text = item.getFormattedPrice()
+            binding.itemCount.text = item.quantityInStock.toString()
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = navigationArgs.itemId
+
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
     }
 
     /**
      * Displays an alert dialog to get the user's confirmation before deleting the item.
      */
     private fun showConfirmationDialog() {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(android.R.string.dialog_alert_title))
-            .setMessage(getString(R.string.delete_question))
-            .setCancelable(false)
+        MaterialAlertDialogBuilder(requireContext()).setTitle(getString(android.R.string.dialog_alert_title))
+            .setMessage(getString(R.string.delete_question)).setCancelable(false)
             .setNegativeButton(getString(R.string.no)) { _, _ -> }
             .setPositiveButton(getString(R.string.yes)) { _, _ ->
                 deleteItem()
-            }
-            .show()
+            }.show()
     }
 
     /**
